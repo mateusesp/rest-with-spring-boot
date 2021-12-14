e778242790de1b8ba5a589017cc174727a480b4d
Mateus Esposito <m.espositof@gmail.com>
2021-12-14 10:52:36 -0300

    add configuration for file upload, creating exception and ResponseVO

--- src/main/java/br/com/erudio/Startup.java
+++ src/main/java/br/com/erudio/Startup.java
@@ -1,14 +1,19 @@
 package br.com.erudio;
 
+import br.com.erudio.config.FileStorageConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+        FileStorageConfig.class
+})
 public class Startup {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Startup.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Startup.class, args);
+    }
 }
---
+++ src/main/java/br/com/erudio/data/vo/v1/UploadFileResponseVO.java
@@ -0,0 +1,43 @@
+package br.com.erudio.data.vo.v1;
+
+import java.io.Serializable;
+
+public class UploadFileResponseVO implements Serializable {
+
+    private String fileName;
+    private String fileDownloadUri;
+    private String fileType;
+    private long size;
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFileDownloadUri() {
+        return fileDownloadUri;
+    }
+
+    public void setFileDownloadUri(String fileDownloadUri) {
+        this.fileDownloadUri = fileDownloadUri;
+    }
+
+    public String getFileType() {
+        return fileType;
+    }
+
+    public void setFileType(String fileType) {
+        this.fileType = fileType;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public void setSize(long size) {
+        this.size = size;
+    }
+}
