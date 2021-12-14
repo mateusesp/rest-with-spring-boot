a93d3030f796de3428f536133ca8cf9bfa2bc8d6
Mateus Esposito <m.espositof@gmail.com>
2021-12-14 11:28:04 -0300

    preparing service to download files

--- src/main/java/br/com/erudio/services/FileStorageService.java
+++ src/main/java/br/com/erudio/services/FileStorageService.java
@@ -1,8 +1,11 @@
 package br.com.erudio.services;
 
 import br.com.erudio.config.FileStorageConfig;
+import br.com.erudio.data.exception.FileNotFoundException;
 import br.com.erudio.data.exception.FileStorageException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
@@ -41,6 +44,22 @@ public class FileStorageService {
             return fileName;
         } catch (Exception exception) {
             throw new FileStorageException("Could not store file " + fileName + ", Please try again", exception);
+        }
+    }
+
+    public Resource loadFileAsResource(String fileName) {
+        try {
+            Path filePath = this.fileStorageLocation.resolve(fileName).normalize();
+            Resource resource = new UrlResource(filePath.toUri());
+
+            if (resource.exists()) {
+                return resource;
+            } else {
+                throw new FileNotFoundException("File not found " + fileName);
+            }
+
+        } catch (Exception exception) {
+            throw new FileNotFoundException("File not found " + fileName, exception);
         }
     }
 }
